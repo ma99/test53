627d0df65f97de185c666e7e5a3f414a618d37db
mas <ma99.dev@gmail.com>
2016-09-19 11:32:33 +0300

    Login with github link added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -68,6 +68,7 @@
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
+                    <a href="{{ url('auth/github') }}">Github</a>
                     <a href="{{ url('/login') }}">Login</a>
                     <a href="{{ url('/register') }}">Register</a>
                 </div>
