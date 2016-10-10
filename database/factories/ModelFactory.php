f9d728d702adfab284a40eff12b615db8387ad7d
mas <ma99.dev@gmail.com>
2016-10-10 10:09:24 +0300

    Dummy data for products table

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -32,3 +32,13 @@ $factory->define(App\Event::class, function (Faker\Generator $faker) {
         
     ];
 });
+
+// dummy data for products table
+$factory->define(App\Product::class, function (Faker\Generator $faker) {
+    return [
+        'title' => $faker->sentence(),
+        'image' => 'http://loremflickr.com/400/300?random='.rand(1, 100),
+        'price' => $faker->numberBetween(3, 100),
+        'description' => $faker->paragraph(2)
+    ];
+});
